8be2f0eacaa21824f782602c6dd920892f0a3340
yar-24 <akhyar24.354@gmail.com>
2022-06-13 16:47:12 +0700

    my first commit

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
 const cookieSession = require("cookie-session");
-const passport = require("passport")
+// const passport = require("passport")
 const bodyParser = require('body-parser')
 const path = require("path")
 const userRouter = require("./src/routes/userRoutes")
 const goalRouter = require("./src/routes/goalRoutes")
 const orderRouter = require("./src/routes/orderRoutes")
 // const passportSetup = require("./src/middleware/passport")
-const authPassport = require("./src/routes/authPassport")
+// const authPassport = require("./src/routes/authPassport")
 const { errorHandler } = require("./src/middleware/errorMiddleware");
 
 // const userPostRoute = require("./src/routes/undang");
@@ -33,7 +33,7 @@ app.use('/fileUploads', express.static(path.join(__dirname, 'fileUploads')))
 app.use("/api/goals", goalRouter);
 app.use("/api/users", userRouter);
 // app.use("/user/post", userPostRoute);
-app.use("/auth", authPassport)
+// app.use("/auth", authPassport)
 app.use("/order", orderRouter)
 app.use(
   cookieSession({
@@ -42,8 +42,8 @@ app.use(
     maxAge: 24 * 60 * 60 * 100,
   })
 );
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(passport.initialize())
+// app.use(passport.session())
 app.use(bodyParser.json())
 
 app.use(errorHandler);
