bb41124c142fd2a928865d5dbfd96679ea08c19f
yar-24 <akhyar24.354@gmail.com>
2022-07-27 12:39:30 +0700

    my commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -58,7 +58,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (userExists) {
     res.status(400);
-    throw new Error("User already exists");
+    throw new Error("Email already exists");
   }
 
   // Hash password
